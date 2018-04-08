9ded23f8e2eff2c7f07f64c38b31c40c3b9da47c
Elliot <gynmi@users.noreply.github.com>
2018-04-08 09:33:03 +0800

    Create work_env.sh

---
+++ work_env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# 下载nmap
+# docker pull uzyexe/nmap:7.12
+# 使用: 
+#    docker run --rm uzyexe/nmap [Scan Type(s)] [Options] {target specification}
+#    docker run --rm uzyexe/nmap -p 80 example.com

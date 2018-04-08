13242502c4390fd635628888722a1743722df74d
Elliot <gynmi@users.noreply.github.com>
2018-04-08 10:20:57 +0800

    Update work_env.sh

--- work_env.sh
+++ work_env.sh
@@ -5,3 +5,10 @@
 # 使用: 
 #    docker run --rm uzyexe/nmap [Scan Type(s)] [Options] {target specification}
 #    docker run --rm uzyexe/nmap -p 80 example.com
+
+# 查看指定镜像的版本
+curl -s -S "https://registry.hub.docker.com/v2/repositories/uzyexe/nmap/tags/" | \
+    sed -e 's/,/,\n/g' -e 's/\[/\[\n/g' | \
+    grep '"name":' | \
+    awk -F\" '{print $4;}' | \
+    sed -e "s/^/${Repo}/"
